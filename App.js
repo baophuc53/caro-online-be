b5c10fc4fc075a35ef6b018525ee59c96f7e6bf2
VO THANH PHONG <vophong1612@gmail.com>
2020-12-07 20:13:29 +0700

    add socket but having some bug

--- App.js
+++ App.js
@@ -1,28 +1,90 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
 const session = require("express-session");
 const logger = require("morgan");
 const mdw = require("./middlewares/route.mdw");
 
-//app.use(cors({origin:"http://localhost:3000",credentials:true}));
+const app = express();
+app.use(cors({origin:"http://localhost:3001",credentials:true}));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  session({ 
-    secret: "keyboard cat", 
-    resave: false, 
-    saveUninitialized: true, 
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
   })
 );
 mdw(app);
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log("Web server running at http://localhost:8000");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let user = null;
+
+app.use((req, res, next) => {
+  if (req.session.token) {
+    const decoded = jwt.verify(req.session.token, "secret");
+    res.locals.user = decoded.dat;
+    user = res.locals.user;
+  }
+  // res.locals.isLoggedIn = req.session.user ? true : false;
+  console.log(user);
+  next();
+});
+
+let socketArr = [];
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(io.of('/').clients);
+  let tokenUser = user ? user : null;
+  let userIDArray = [];
+  // console.log(tokenUser);
+  if (tokenUser) {
+    tokenUser.socketID = socket.id;
+    socketArr.push(tokenUser);
+    for (let i in socketArr) {
+      userIDArray.push({
+        username: socketArr[i].username,
+        nickname: socketArr[i].nickname,
+        socketID: socketArr[i].socketID,
+      });
+    }
+    console.log(userIDArray);
+    io.sockets.emit("send-online-user-list", userIDArray);
+  }
+  socket.on("disconnect", () => {
+    let usernameDisconnect = [];
+    console.log("user disconnected");
+    if (socketArr.length > 0) {
+      for (let i in socketArr) {
+        if (socketArr[i].socketID == socket.id) {
+          socketArr.splice(i, 1);
+          break;
+        }
+      }
+    }
+    for (let i in userIDArray) {
+      if (userIDArray[i].socketID == socket.id) {
+        usernameDisconnect.push(userIDArray[i]);
+        break;
+      }
+    }
+    io.sockets.emit("a user offline", usernameDisconnect);
   });
+});
+
+server.listen(process.env.PORT || 8000, () => {
+  console.log("Web server running at http://localhost:8000");
+});
 
 module.exports = app;
-
--- routes/user.route.js
+++ routes/user.route.js
@@ -8,9 +8,10 @@ router.post("/", async (req, res) => {
     const { username, password } = req.body;
     const dat = await userModel.loadByUserName(username);
     if (dat && await bcrypt.compare(password, dat.password)) {
-            const token = jwt.sign({ id: dat.id, username }, "secret");
+            const token = jwt.sign({dat}, "secret");
             req.session.token = token;
-            console.log(req.session);
+            // console.log(req.session);
+
             res.json({
                 code: 0,
                 data: {
