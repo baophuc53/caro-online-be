db10d43a6555c6357dd9eeb790913c8faeb90da9
VO THANH PHONG <vophong1612@gmail.com>
2020-12-09 00:23:14 +0700

    fix conflictt

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const session = require("express-session");
 const logger = require("morgan");
 const mdw = require("./middlewares/route.mdw");
@@ -12,54 +12,71 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "http://localhost:3000",
-    credentials: true
+    credentials: true,
   },
 });
-app.use(cors({origin:"http://localhost:3000",credentials:true}));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({extended:true}));
-const _session = session({ 
-  secret: "keyboard cat", 
-  resave: false, 
-  saveUninitialized: true, 
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+const _session = session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+});
 app.use(_session);
 
 let user;
-app.use((req, res, next) => {
-  if (req.session.token) {
+
+app.post("/", (req, res) => {
+  const token = req.body.token;
+  if (token) {
     const decoded = jwt.verify(req.session.token, "secret");
     user = decoded.dat;
   }
-  // res.locals.isLoggedIn = req.session.user ? true : false;
-  // console.log(user);
-  next();
+  console.log(user);
 });
 
 let socketArr = [];
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  // console.log(socket.handshake.sessionStore.sessions);
   let tokenUser = user ? user : null;
   if (tokenUser) {
-    tokenUser.socketID = socket.id;
-    socketArr.push(tokenUser);
-    // console.log(socketArr);
+    let exist = 1;
+    if (socketArr.length > 0) {
+      for (let i in socketArr) {
+        if (socketArr[i].id == tokenUser.id) {
+          socketArr[i].exist = socketArr[i].exist + 1;
+          exist = socketArr[i].exist;
+          break;
+        }
+      }
+    }
+    if (exist == 1) {
+      tokenUser.socketID = socket.id;
+      tokenUser.exist = exist;
+      socketArr.push(tokenUser);
+    }
+    console.log(socketArr);
     io.sockets.emit("send-online-user-list", socketArr);
   }
   socket.on("disconnect", () => {
     console.log("--------------user disconnected----------------------");
-    // console.log(socket.id);
+    console.log(socket.id);
     if (socketArr.length > 0) {
       for (let i in socketArr) {
-        // console.log(socketArr[i].socketID);
+        console.log(socketArr[i].socketID);
         if (socketArr[i].socketID == socket.id) {
+          if (socketArr[i].exist == 1){
           socketArr.splice(i, 1);
           break;
+          }
+          else{
+            socketArr[i].exist = socketArr[i].exist - 1;
+          }
         }
       }
     }
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,52 +5,53 @@ const jwt = require("jsonwebtoken");
 
 //login
 router.post("/", async (req, res) => {
-    const { username, password } = req.body;
-    const dat = await userModel.loadByUserName(username);
-    if (dat && await bcrypt.compare(password, dat.password)) {
-            const token = jwt.sign({ dat }, "secret");
-            req.session.token = token;
-            // console.log(req.session);
-            res.json({
-                code: 0,
-                data: {
-                    token,
-                },
-            });
-    } else {
-        res.json({
-            code: 1,
-            message: "login fail",
-        });
-    };
+  const { username, password } = req.body;
+  const dat = await userModel.loadByUserName(username);
+  if (dat && (await bcrypt.compare(password, dat.password))) {
+    const token = jwt.sign({ dat: {id: dat.id, username, nickname: dat.nickname } }, "secret");
+    req.session.token = token;
+    // console.log(req.session);
+
+    res.json({
+      code: 0,
+      data: {
+        token,
+      },
+    });
+  } else {
+    res.json({
+      code: 1,
+      message: "login fail",
+    });
+  }
 });
 
 //register
 router.put("/", async (req, res) => {
-    const entity = req.body;
-    bcrypt.hash(entity.password, 10).then( async (hash) => {
-        console.log(hash);
-        entity.password = hash;
-        await userModel
-            .add(entity)
-            .then((response) => {
-                res.json({
-                    code: 0,
-                    data: {
-                        id: response.insertId,
-                    },
-                });
-            })
-            .catch((err) => {
-                console.log(err);
-                res.json({
-                    code: 1,
-                    data: {
-                        message: "Fail to add user!",
-                    },
-                });
-            });
-    });
+  const entity = req.body;
+  bcrypt.hash(entity.password, 10).then(async (hash) => {
+    console.log(hash);
+    entity.password = hash;
+    await userModel
+      .add(entity)
+      .then((response) => {
+        res.json({
+          code: 0,
+          data: {
+            id: response.insertId,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          code: 1,
+          data: {
+            message: "Fail to add user!",
+          },
+        });
+      });
+  });
 });
 
 module.exports = router;
